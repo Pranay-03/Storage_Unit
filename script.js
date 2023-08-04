0b0c25922a9816ab45887744513aecbcc403fe9c
Arigela Pranay kumar <arigelapranaykumar123@gmail.com>
2023-08-04 21:33:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("login-form");
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        
+        // Replace these with your actual predefined username and password
+        const predefinedUsername = "username";
+        const predefinedPassword = "password";
+        
+        if (username === predefinedUsername && password === predefinedPassword) {
+            //alert("Login successful!");
+            // Redirect to the web app after successful login
+            window.location.href = "view.html"; // Replace with your web app's URL
+        } else {
+            alert("Login failed. Please check your credentials.");
+        }
+    });
+});
